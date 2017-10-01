85d5ab2b5f9847363ecb806694d9285cd8f51e8b
Yothri <admin@yothri.com>
2017-10-01 23:38:26 +0200

    Fixed probably all crashing issues!

--- src/FN/dllmain.cpp
+++ src/FN/dllmain.cpp
@@ -2,7 +2,6 @@
 
 using namespace SDK;
 
-UWorld *pUWorld;
 
 void init(HMODULE hModule)
 {
@@ -13,73 +12,83 @@ void init(HMODULE hModule)
 	DWORD64 instructionOffset = MemTools::Pattern("\x48\x8B\x15\x00\x00\x00\x00\x45\x33\xC0\xE8\x00\x00\x00\x00", "xxx????xxxx????", 3);
 	DWORD64 UWorldOffset = *reinterpret_cast<DWORD*>(instructionOffset) + instructionOffset + sizeof(DWORD);
 
-	pUWorld = *reinterpret_cast<UWorld**>(UWorldOffset);
-
 	while (!GetAsyncKeyState(VK_DELETE) & 1)
 	{
 		system("cls");
-		if (!pUWorld)
-			continue;
 
-		if (!pUWorld->PersistentLevel)
+		UWorld *pUWorld = *reinterpret_cast<UWorld**>(UWorldOffset);
+		if (pUWorld)
+			printf("World is valid @ 0x%I64X\n", pUWorld);
+		else
+		{
+			printf("World is invalid!\n");
 			continue;
+		}
 
-		if (!pUWorld->OwningGameInstance)
+		if (pUWorld->OwningGameInstance)
+			printf("World->OwningGameInstance is valid @ 0x%I64X\n", pUWorld->OwningGameInstance);
+		else
+		{
+			printf("World->OwningGameInstance is invalid!\n");
 			continue;
-
-		if (pUWorld->OwningGameInstance->LocalPlayers.Num() < 1)
-			continue;
+		}
 
 		if (!pUWorld->OwningGameInstance->LocalPlayers.IsValidIndex(0))
 			continue;
 
-		ULocalPlayer *localPlayer = pUWorld->OwningGameInstance->LocalPlayers[0];
-		if (!localPlayer)
+		ULocalPlayer *pLocalPlayer = pUWorld->OwningGameInstance->LocalPlayers[0];
+		if (!pLocalPlayer)
 			continue;
 
-		if (!localPlayer->PlayerController)
+		if (!pLocalPlayer->ViewportClient)
 			continue;
 
-		AFortPawn *localPlayerPawn = static_cast<AFortPawn*>(localPlayer->PlayerController->Pawn);
-		if (!localPlayerPawn)
+		if (!pLocalPlayer->ViewportClient->World)
 			continue;
 
-		if (!localPlayerPawn->RootComponent)
+		UWorld *pRealWorld = pLocalPlayer->ViewportClient->World;
+		if (!pRealWorld)
 			continue;
 
-		FVector localPlayerLocation = localPlayerPawn->RootComponent->Location;
-
-		AFortPlayerStateAthena *localPlayerState = static_cast<AFortPlayerStateAthena*>(localPlayerPawn->PlayerState);
-		if (!localPlayerState)
+		if (!pRealWorld->OwningGameInstance)
 			continue;
 
-		for (int i = 0; i < pUWorld->PersistentLevel->AActors.Num(); i++)
+		if (!pRealWorld->OwningGameInstance->LocalPlayers[0])
+			continue;
+
+		if (!pRealWorld->OwningGameInstance->LocalPlayers[0]->PlayerController)
+			continue;
+
+		AFortPawn *pLocalPlayerPawn = static_cast<AFortPawn*>(pRealWorld->OwningGameInstance->LocalPlayers[0]->PlayerController->Pawn);
+		if (!pLocalPlayerPawn)
+			continue;
+
+		if (!pRealWorld->PersistentLevel)
+			continue;
+
+		for (int i = 0; i < pRealWorld->PersistentLevel->AActors.Num(); i++)
 		{
-			AFortPawn *pPlayerPawn = static_cast<AFortPawn*>(pUWorld->PersistentLevel->AActors[i]);
-			if (!pPlayerPawn || pPlayerPawn == localPlayerPawn)
+			if (!pRealWorld->PersistentLevel->AActors.IsValidIndex(i))
 				continue;
 
-			if (!pPlayerPawn->RootComponent)
+			AFortPawn * pPlayerPawn = static_cast<AFortPawn*>(pRealWorld->PersistentLevel->AActors[i]);
+			if (!pPlayerPawn)
 				continue;
 
-			if (pPlayerPawn->GetName().find("PlayerPawn_Athena_C") == std::string::npos)
+			if (pPlayerPawn->GetName().find("PlayerPawn") == std::string::npos)
 				continue;
 
-			AFortPlayerStateAthena *pPlayerState = static_cast<AFortPlayerStateAthena*>(pPlayerPawn->PlayerState);
+			AFortPlayerStateAthena * pPlayerState = static_cast<AFortPlayerStateAthena*>(pPlayerPawn->PlayerState);
 			if (!pPlayerState)
 				continue;
 
-			if (!pPlayerState->PlayerName.IsValid())
-				continue;
-
-			FVector playerLocation = pPlayerPawn->RootComponent->Location;
-
-			double distance = sqrt(pow(localPlayerLocation.X - playerLocation.X, 2) + pow(localPlayerLocation.Y - playerLocation.Y, 2) + pow(localPlayerLocation.Z - playerLocation.Z, 2)) * 0.0254f;
-
-			wprintf(L"Player: %s, Dist: %.2f\n", pPlayerState->PlayerName.c_str(), distance);
+			if (pPlayerState->PlayerName.IsValid())
+			{
+				wprintf(L"Player: %s | Health: %.0f | Shield: %.0f | Distance: %.2f meters.\n", pPlayerState->PlayerName.c_str(), pPlayerState->CurrentHealth, pPlayerState->CurrentShield, pPlayerPawn->GetDistanceTo(pLocalPlayerPawn) / 100.f);
+			}
 		}
 
-		Sleep(10);
+		Sleep(1000);
 	}
 
 	printf("Ejecting...\n");
