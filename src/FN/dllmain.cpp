98cbadb396478168318a6cb4ac660272f624b015
Yothri <admin@yothri.com>
2017-10-01 17:29:44 +0200

    Now retrieving Objects and Names before using them xD Showing Players names works like a charm I guess.

--- src/FN/dllmain.cpp
+++ src/FN/dllmain.cpp
@@ -9,10 +9,18 @@ void init(HMODULE hModule)
 	AllocConsole();
 	freopen("CONOUT$", "w", stdout);
 
+	const auto addressObj = MemTools::Pattern("\x48\x8D\x0D\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE8\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x48\x8B\xD6", "xxx????x????x????x????xxx");
+	const auto offsetObj = *reinterpret_cast<uint32_t*>(addressObj + 3);
+	UObject::GObjects = reinterpret_cast<FUObjectArray*>(addressObj + 7 + offsetObj);
+
+	const auto addressNames = MemTools::Pattern("\x48\x8B\x05\x00\x00\x00\x00\x48\x85\xC0\x75\x50\xB9\x00\x00\x00\x00\x48\x89\x5C\x24", "xxx????xxxxxx????xxxx");
+	const auto offsetNames = *reinterpret_cast<uint32_t*>(addressNames + 3);
+	FName::GNames = *reinterpret_cast<TNameEntryArray**>(addressNames + 7 + offsetNames);
+
 	DWORD64 baseAddr = reinterpret_cast<DWORD64>(GetModuleHandleA(nullptr));
 	DWORD64 instructionOffset = MemTools::Pattern("\x48\x8B\x15\x00\x00\x00\x00\x45\x33\xC0\xE8\x00\x00\x00\x00", "xxx????xxxx????", 3);
 	DWORD64 UWorldOffset = *reinterpret_cast<DWORD*>(instructionOffset) + instructionOffset + sizeof(DWORD);
-	
+
 	pUWorld = *reinterpret_cast<UWorld**>(UWorldOffset);
 
 	while (!GetAsyncKeyState(VK_DELETE) & 1)
@@ -20,7 +28,7 @@ void init(HMODULE hModule)
 		system("cls");
 		if (!pUWorld || !pUWorld->OwningGameInstance || !pUWorld->PersistentLevel)
 			continue;
-		
+
 		ULocalPlayer *localPlayer = pUWorld->OwningGameInstance->LocalPlayers[0];
 		if (!localPlayer || !localPlayer->PlayerController)
 			continue;
@@ -49,12 +57,13 @@ void init(HMODULE hModule)
 			if (!pPlayerState->PlayerName.IsValid())
 				continue;
 
-			wprintf(L"Player: %d has name: %s", i, pPlayerState->PlayerName.c_str());
+			wprintf(L"Player: %d has name: %s\n", i, pPlayerState->PlayerName.c_str());
 		}
 
 		Sleep(10);
 	}
 
+	printf("Ejecting...\n");
 	FreeConsole();
 	FreeLibraryAndExitThread(hModule, 0);
 }
